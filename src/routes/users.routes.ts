ae84d86f2fd79dca249f3498d01bc0dc6313aea4
Herli Khoury <92793769+HerliKhoury@users.noreply.github.com>
2023-07-28 17:07:31 -0300

    feat: middleware ensureTokenIsValid

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,10 +1,11 @@
 import { Router } from "express";
+import { ensureTokenIsValid } from "../middlewares/ensureTokenIsValid.middleware";
 
 
 export const userRoutes: Router = Router();
 
-userRoutes.post("", );
-userRoutes.get("", );
-userRoutes.patch("/:id",);
-userRoutes.delete("/:id",);
-userRoutes.get("/:id/customers", ); 
+userRoutes.post("",);
+userRoutes.get("",);
+userRoutes.patch("/:id", ensureTokenIsValid);
+userRoutes.delete("/:id", ensureTokenIsValid);
+userRoutes.get("/:id/customers", ensureTokenIsValid); 
